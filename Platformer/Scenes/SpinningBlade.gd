bfe0bfc801e1930e924f243cf8661a60182c5a66
mossy2 <100237161+mossy2@users.noreply.github.com>
2022-09-01 14:57:11 +1200

    igfuhighgft

--- Platformer/Scenes/SpinningBlade.gd
+++ Platformer/Scenes/SpinningBlade.gd
@@ -1,8 +1,8 @@
 extends Node2D
-
+export (int) var follow_speed = 50
 
 func _ready():
 	$AnimationPlayer.play("Spin")
 	
 func _process(delta):
-	$Path2D/PathFollow2D.offset += 50 * delta
+	$Path2D/PathFollow2D.offset += follow_speed * delta
