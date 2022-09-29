4a0c3ea4bb0d5457e0b9bb904e7d1bb631323527
mossy2 <100237161+mossy2@users.noreply.github.com>
2022-09-30 10:55:47 +1300

    gggg

--- Platformer/Scenes/Player.gd
+++ Platformer/Scenes/Player.gd
@@ -84,4 +84,5 @@ func _on_Area2D_body_entered(body):
 
 func _on_DeathZone_area_entered(area):
 	if area.is_in_group("Deadly"):
-		if GameStats.check_reset()
+		if GameStats.check_reset() == false:
+			global_position = GameStats.get_spawn().global_position
