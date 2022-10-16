8196caa78125814fe45ff91e040b9e444afffff8
mossy2 <100237161+mossy2@users.noreply.github.com>
2022-10-17 12:26:24 +1300

    ggg

---
+++ Platformer/Scenes/SwingingTrap.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _ready():
+	$AnimationPlayer.play("Swing")
+
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		print("player dead")
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position
+
+
