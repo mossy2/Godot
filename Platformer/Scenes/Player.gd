76c409d57f2f0ffe5e20877ad616ab0909e7f4c0
mossy2 <100237161+mossy2@users.noreply.github.com>
2022-11-01 10:01:53 +1300

    Sound effects

--- Platformer/Scenes/Player.gd
+++ Platformer/Scenes/Player.gd
@@ -58,6 +58,7 @@ func _physics_process(delta):
 	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		SoundPlayer.play_sound_effect("jump")
 		player_state = state.STARTJUMP
 	elif velocity.x != 0:
 		player_state = state.RUNNING
@@ -84,5 +85,6 @@ func _on_Area2D_body_entered(body):
 
 func _on_DeathZone_area_entered(area):
 	if area.is_in_group("Deadly"):
+		SoundPlayer.play_sound_effect("dead")
 		if GameStats.check_reset() == false:
 			global_position = GameStats.get_spawn().global_position
--- Platformer/Scripts/sound_player.gd
+++ Platformer/Scripts/sound_player.gd
@@ -3,14 +3,14 @@ extends Node
 onready var music = AudioStreamPlayer.new()
 
 var music_tracks = {
-	"title_track":
-	
+	"main":"res://sfx/music/Take_On_Me_8_Bit_Remix_Cover_Version_Tribute_to.mp3", 
+}
 
 
-
-
-
-
+var sound_effects = {
+	"jump":"res://sfx/mixkit-player-jumping-in-a-video-game-2043.wav",
+	"dead":"res://sfx/8-bit-video-game-death-sound-effect.mp3",
+}
 
 
 
@@ -30,3 +30,12 @@ func _ready():
 	add_child(music)
 	music.play()
 	
+
+
+func play_sound_effect(sfx):
+	var sound = AudioStreamPlayer.new()
+	sound.stream = load(sound_effects[sfx])
+	add_child(sound)
+	sound.play()
+	yield(sound,"finished")
+	sound.queue_free()
